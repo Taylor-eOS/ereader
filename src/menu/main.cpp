a7e6ef27934f3043f3a8a55c4208471efecbde1b
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2026-03-27 17:17:18 +0100

    Menu is just book list

--- src/menu/main.cpp
+++ src/menu/main.cpp
@@ -2,6 +2,7 @@
 #include <SPI.h>
 #include <Fonts/FreeSerif9pt7b.h>
 #include <GxEPD2_BW.h>
+
 #define EPD_PIN_CS   41
 #define EPD_PIN_DC   42
 #define EPD_PIN_RST  2
@@ -15,33 +16,48 @@
 #define EPD_H        300
 #define MARGIN_X     12
 #define MARGIN_Y     12
+
 static GxEPD2_BW<GxEPD2_420_GDEY042T81, GxEPD2_420_GDEY042T81::HEIGHT> epd(
     GxEPD2_420_GDEY042T81(EPD_PIN_CS, EPD_PIN_DC, EPD_PIN_RST, EPD_PIN_BUSY)
 );
+
 enum Screen {
     SCREEN_MENU,
-    SCREEN_READING
+    SCREEN_READING,
+    SCREEN_SETTINGS
 };
+
 static Screen currentScreen = SCREEN_MENU;
-static const char* menuItems[] = {
-    "Read Demo Book",
-    "Settings"
+
+struct Book {
+    const char* title;
+    const char* text;
 };
-static const int menuCount = sizeof(menuItems) / sizeof(menuItems[0]);
+
+static const Book books[] = {
+    { "Lorem Ipsum",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris "
+      "nisi ut aliquip ex ea commodo consequat." },
+};
+
+static const int bookCount = sizeof(books) / sizeof(books[0]);
+static const int menuCount = bookCount + 1;
+static const int MENU_SETTINGS_INDEX = menuCount - 1;
+
 static int selectedIndex = 0;
+static int openBookIndex = 0;
 static bool needsRedraw = true;
 static int16_t lineHeight = 18;
-static const char* demoText =
-"Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
-"Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
-"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris "
-"nisi ut aliquip ex ea commodo consequat.";
 static int textOffset = 0;
+
 bool buttonPressed(uint8_t pin);
 void updateInput();
 void redrawScreen();
 void drawMenu();
 void drawReading();
+void drawSettings();
 
 void setup() {
     Serial.begin(115200);
@@ -84,19 +100,21 @@ bool buttonPressed(uint8_t pin) {
 void updateInput() {
     if (currentScreen == SCREEN_MENU) {
         if (buttonPressed(BTN_UP)) {
-            selectedIndex--;
-            if (selectedIndex < 0) selectedIndex = menuCount - 1;
+            selectedIndex = (selectedIndex - 1 + menuCount) % menuCount;
             needsRedraw = true;
         }
         if (buttonPressed(BTN_DOWN)) {
-            selectedIndex++;
-            if (selectedIndex >= menuCount) selectedIndex = 0;
+            selectedIndex = (selectedIndex + 1) % menuCount;
             needsRedraw = true;
         }
         if (buttonPressed(BTN_SELECT)) {
-            if (selectedIndex == 0) {
-                currentScreen = SCREEN_READING;
+            if (selectedIndex == MENU_SETTINGS_INDEX) {
+                currentScreen = SCREEN_SETTINGS;
+                needsRedraw = true;
+            } else {
+                openBookIndex = selectedIndex;
                 textOffset = 0;
+                currentScreen = SCREEN_READING;
                 needsRedraw = true;
             }
         }
@@ -114,6 +132,11 @@ void updateInput() {
             currentScreen = SCREEN_MENU;
             needsRedraw = true;
         }
+    } else if (currentScreen == SCREEN_SETTINGS) {
+        if (buttonPressed(BTN_SELECT)) {
+            currentScreen = SCREEN_MENU;
+            needsRedraw = true;
+        }
     }
 }
 
@@ -123,13 +146,15 @@ void redrawScreen() {
         drawMenu();
     } else if (currentScreen == SCREEN_READING) {
         drawReading();
+    } else if (currentScreen == SCREEN_SETTINGS) {
+        drawSettings();
     }
     epd.display(false);
 }
 
 void drawMenu() {
     int y = MARGIN_Y + lineHeight;
-    for (int i = 0; i < menuCount; i++) {
+    for (int i = 0; i < bookCount; i++) {
         int boxY = y - lineHeight + 4;
         int boxH = lineHeight + 6;
         if (i == selectedIndex) {
@@ -139,14 +164,35 @@ void drawMenu() {
             epd.setTextColor(GxEPD_BLACK);
         }
         epd.setCursor(MARGIN_X, y);
-        epd.print(menuItems[i]);
+        epd.print(books[i].title);
         y += lineHeight + 10;
     }
+    int boxY = y - lineHeight + 4;
+    int boxH = lineHeight + 6;
+    if (MENU_SETTINGS_INDEX == selectedIndex) {
+        epd.fillRect(MARGIN_X - 4, boxY, EPD_W - (MARGIN_X * 2), boxH, GxEPD_BLACK);
+        epd.setTextColor(GxEPD_WHITE);
+    } else {
+        epd.setTextColor(GxEPD_BLACK);
+    }
+    epd.setCursor(MARGIN_X, y);
+    epd.print("Settings");
 }
 
 void drawReading() {
     epd.setTextColor(GxEPD_BLACK);
     epd.setTextWrap(true);
     epd.setCursor(MARGIN_X, MARGIN_Y + lineHeight);
-    epd.print(demoText + textOffset);
+    const char* text = books[openBookIndex].text;
+    int textLen = strlen(text);
+    int offset = textOffset < textLen ? textOffset : textLen;
+    epd.print(text + offset);
+}
+
+void drawSettings() {
+    epd.setTextColor(GxEPD_BLACK);
+    epd.setCursor(MARGIN_X, MARGIN_Y + lineHeight);
+    epd.print("Settings");
+    epd.setCursor(MARGIN_X, MARGIN_Y + lineHeight * 2 + 10);
+    epd.print("Press select to go back.");
 }
